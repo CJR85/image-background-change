ca584a9d7ce0e69274df2c6b16302d2b258973b3
Calvin Roberts <roberts.calvin85@gmail.com>
2021-06-25 22:41:27 +0100

    Add variable to line id.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const ImgBox = document.querySelector('.img-box'),
   imgWrap = document.querySelector('.img-wrap'),
   originalImg = document.getElementById('originalImg'),
+  line = document.getElementById('line'),
   leftSpace = ImgBox.offsetLeft;
 
 originalImg.style.width = ImgBox.offsetWidth + 'px';
@@ -8,4 +9,5 @@ originalImg.style.width = ImgBox.offsetWidth + 'px';
 ImgBox.onmousemove = function (e) {
   const boxWidth = e.pageX - leftSpace + 'px';
   imgWrap.style.width = boxWidth;
+  line.style.left = boxWidth;
 };
