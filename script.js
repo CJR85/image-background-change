1dcf86f0e37773803d4ed7ece191f3ec9d75172d
Calvin Roberts <roberts.calvin85@gmail.com>
2021-06-25 22:34:22 +0100

    Add variable for originalImg id.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const ImgBox = document.querySelector('.img-box'),
   imgWrap = document.querySelector('.img-wrap'),
+  originalImg = document.getElementById('originalImg'),
   leftSpace = ImgBox.offsetLeft;
+
+originalImg.style.width = ImgBox.offsetWidth + 'px';
 
 ImgBox.onmousemove = function (e) {
   const boxWidth = e.pageX - leftSpace + 'px';
